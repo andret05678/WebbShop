7bbaa0176c572e93308d70a177d8ed83968e5844
acnenerd <andreta@ug.kth.se>
2025-09-26 15:52:11 +0200

    bajs

--- src/main/java/com/Model/Order.java
+++ src/main/java/com/Model/Order.java
@@ -1,4 +1,31 @@
 package com.Model;
 
 public class Order {
+    private int id;
+    private int userId;
+    private String status;
+
+    public Order(int id, int userId, String status) {
+        this.id = id;
+        this.userId = userId;
+        this.status = status;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public int getUserId() { return userId; }
+    public void setUserId(int userId) { this.userId = userId; }
+
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", userId=" + userId +
+                ", status='" + status + '\'' +
+                '}';
+    }
 }
