b6100c2b8051e6a5c8646cc98388b9025f2611b9
acnenerd <andreta@ug.kth.se>
2025-09-28 01:02:20 +0200

    Update UserDbImp.java

--- src/main/java/com/DB/imp/UserDbImp.java
+++ src/main/java/com/DB/imp/UserDbImp.java
@@ -1,5 +1,31 @@
 package com.DB.imp;
 
+import com.Model.User;
+
 public class UserDbImp implements com.DB.UserDB {
 
+    @Override
+    public User findByEmail(String email) {
+        return null;
+    }
+
+    @Override
+    public User findById(int id) {
+        return null;
+    }
+
+    @Override
+    public User insert(User user) {
+        return null;
+    }
+
+    @Override
+    public boolean update(User user) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(int id) {
+        return false;
+    }
 }
